f0479fcb89ca7b471bfde1d587ef0773de3809ea
DengSihan <root@dsh.email>
2022-11-02 08:12:57 +0000

    feat: autoupdate without refresh page

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,41 @@ import NotificationsWrapper from '~/components/notifications-wrapper.vue';
 import Dashboard from '~/components/dashboard.vue';
 
 import { useMeta } from 'vue-meta';
+import { onMounted, onBeforeUnmount } from 'vue';
+import { randomIntFromInterval } from '~/utils/helpers.js';
+import axios from 'axios';
+import { version } from '../package.json';
+import { notify } from '@kyvg/vue3-notification';
 
 useMeta({
 	titleTemplate: '%s - 广播云',
-})
+});
+
+let clientUpdateChecker = null;
+
+onMounted(() => {
+	clientUpdateChecker = setInterval(
+		() => {
+			axios.get(`${location.origin}/app.json?time=${(new Date()).getTime()}`)
+				.then(({ data }) => {
+					if (data.version !== version) {
+						notify({
+							title: '发现新版本',
+							text: '即将刷新页面以更新',
+						});
+						setTimeout(() => {
+							location.reload();
+						}, randomIntFromInterval(3000, 6000));
+					}
+				});
+		},
+		randomIntFromInterval(1000 * 60 * 2, 1000 * 60 * 5)
+	);
+});
+
+onBeforeUnmount(() => {
+	if (clientUpdateChecker) {
+		clearInterval(clientUpdateChecker);
+	}
+});
 </script>
