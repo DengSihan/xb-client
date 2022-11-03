c9a9e733787e245971cf5db3702caed0c3de13aa
DengSihan <root@dsh.email>
2022-11-03 05:09:18 +0000

    feat: rewrite client update checker

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ import { useMeta } from 'vue-meta';
 import { onMounted, onBeforeUnmount } from 'vue';
 import { randomIntFromInterval } from '~/utils/helpers.js';
 import axios from 'axios';
-import { version } from '../package.json';
+import { last_updated_at } from '../package.json';
 import { notify } from '@kyvg/vue3-notification';
 
 useMeta({
@@ -35,7 +35,7 @@ onMounted(() => {
 		() => {
 			axios.get(`${location.origin}/app.json?time=${(new Date()).getTime()}`)
 				.then(({ data }) => {
-					if (data.version !== version) {
+					if (data.last_updated_at !== last_updated_at) {
 						notify({
 							title: '发现新版本',
 							text: '即将刷新页面以更新',
